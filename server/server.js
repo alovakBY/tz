e48b0527a65e2d0aace7a43a8f4705193c571786
Артем Лосев <alovakby@gmail.com>
2022-03-24 17:54:04 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const feathers = require("@feathersjs/feathers");
 const express = require("@feathersjs/express");
-const service = require("feathers-mongodb");
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 
 const app = express(feathers());
 const port = 8000;
 
-// app.listen(port, () => {
-//    console.log("We are live on " + port);
-// });
+// const mongoClient = MongoClient.connect(
+//    "mongodb://localhost:27017",
+//    (err, database) => {
+//       if (err) return console.log(err);
+//       require("./app/routes")(app, database);
+//       app.listen(port, () => {
+//          console.log("We are live on " + port);
+//       });
+//       database.close();
+//    }
+// );
 
 // class CardsService {
 //    constructor() {
@@ -38,25 +46,12 @@ const port = 8000;
 //    }
 // }
 
-// app.use("/cards", new CardsService());
+app.use("/cards", (req, res) => {
+   console.log(req);
+   res.send("hello");
+});
 
-// app.use(express.errorHandler());
-
-app.use("/cards", service({}));
-
-MongoClient.connect("mongodb://0.0.0.0:27017")
-   .then(function (client) {
-      // Set the model now that we are connected
-      app.service("cards").Model = client.db("feathers").collection("cards");
-
-      // Now that we are connected, create a dummy Message
-      app.service("cards")
-         .create({
-            text: "Message created on server",
-         })
-         .then((message) => console.log("Created message", message));
-   })
-   .catch((error) => console.error(error));
+// app.use("/cards");
 
 app.listen(port, () => {
    console.log(`Feathers server listening on port ${port}`);
